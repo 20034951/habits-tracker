f2706b77723e9d198a9630686af3f8a8538408a0
20034941 <20034941@galileo.edu>
2025-03-20 21:15:53 -0600

    Actividad 04 Programacion Avanzada Semana 04

--- frontend/app/habits.tsx
+++ frontend/app/habits.tsx
@@ -1,11 +1,31 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchHabitsThunk, markAsDoneThunk } from '../features/habit/habitSlice';
+import { RootState, AppDispatch } from '../redux/store';
+import { toast } from 'react-hot-toast';
+
+
 type Habit = {
     _id: string;
     title: string;
     description: string;
+    days: number;
     createdAt: string;
+    startedAt: string;
+    lastUpdate: string;
+    lastDone: string;
 }
 type HabitsProps = {
     habits: Habit[];
+}
+
+const handleMarkAsDone = async (id: string, dispatch: AppDispatch) => {
+    try{
+        await dispatch(markAsDoneThunk(id)).unwrap();
+        toast.success("Habit marked as done!");
+        dispatch(fetchHabitsThunk());
+    }catch(error){
+        toast.error(`${error}`);
+    }
 }
 
 const calcularProgreso = (createdAt: string) => {
@@ -21,14 +41,25 @@ const calcularProgreso = (createdAt: string) => {
     return `${progress.toFixed(2)}%`;
 }
 
+const calculateProgress = (days: number): number => {
+    return Math.min((days / 66) * 100, 100);
+}
+
 export default function Habits({habits}: HabitsProps){
+
+    const dispatch = useDispatch<AppDispatch>();
+    const status = useSelector( (state: RootState) => state.habits.status );
+    const error = useSelector( (state: RootState) => state.habits.error );
+
     return (
         <div className="pt-12 flex flex-col gap-4 items-center">
             <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-md mt-8">
                 <h1 className="text-2xl text-black font-bold mb-4">Lista de Hábitos</h1>
                 <ul className="space-y-4">
                     {habits.map((habit) => {
-                    const progreso = calcularProgreso(habit.createdAt);
+                    //const progreso = calcularProgreso(habit.createdAt);
+                    //<progress className='bg-orange-500 h-2 rounded-full' value={progress} max='100'></progress>    
+                    const progress = calculateProgress(habit.days);
                     return (
                         <li key={habit._id} className="flex items-center justify-between">
                         <div className="w-96 h-40 bg-white shadow-lg rounded-2xl p-4 flex justify-between items-center border border-gray-200">
@@ -38,12 +69,13 @@ export default function Habits({habits}: HabitsProps){
                                 <p className="text-xs text-gray-500 mt-1">Fecha de creación: {new Date(habit.createdAt).toLocaleDateString()}</p>
                     
                                 <div className="w-full bg-gray-200 rounded-full h-2 mt-2">
-                                    <div className="bg-orange-500 h-2 rounded-full" style={{ width: progreso }}></div>
+                                    <div className="bg-orange-500 h-2 rounded-full" style={{ width: progress }}></div>
                                 </div>
                             </div>                
-                            <button className="ml-4 w-24 h-10 bg-blue-600 text-white text-sm font-medium rounded-lg hover:bg-blue-400 transition flex items-center justify-center">
-                            Finalizado
-                            </button>
+                            <button className="ml-4 w-24 h-10 bg-blue-600 text-white text-sm font-medium rounded-lg hover:bg-blue-400 transition flex items-center justify-center"
+                            onClick={() => handleMarkAsDone(habit._id, dispatch)}>
+                                {status[habit._id] === 'loading' ? 'Processing' : 'Mark as Done'}
+                            </button>    
                         </div>
                         </li>
                     );
--- frontend/features/habit/habitSlice.ts
+++ frontend/features/habit/habitSlice.ts
@@ -1,26 +1,51 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchHabits } from './habitApi';
+import { fetchHabits, markHabitAsDone } from './habitApi';
 
 type Habit = {
     _id: string;
     title: string;
     description: string;
+    days: number;
     createdAt: string;
+    startedAt: string;
+    lastUpdate: string;
+    lastDone: string;
 }
 
 type HabitState = {
     habits: Habit[];
+    status: Record<string, 'idle' | 'loading' | 'success' | 'failed'>;
+    error: Record<string, string | null>;
+
 }
 
 const initialState: HabitState = {
-    habits: []
+    habits: [],
+    status: {},
+    error: {}
 }
 
 export const fetchHabitsThunk = createAsyncThunk('habit/fetchHabits', async () => {
     const response = await fetchHabits();
-    console.log('response -> ',response);
+    console.log('response fetchHabitsThunk habitSlice -> ', response);
     return response;
-})
+});
+
+export const markAsDoneThunk = createAsyncThunk('habit/done', async (id: string, { rejectWithValue }) => {
+    try{
+        const response = await markHabitAsDone(id);
+        console.log('response markAsDoneThunk  habitSlice -> ', response);
+
+        if(response.message === "Habit restarted"){
+            return rejectWithValue(response.message);
+        }
+
+        return response.message;
+    }catch(error){
+        return rejectWithValue('Failed to mark habit as done');
+    }
+    
+});
 
 const habitSlice = createSlice({
     name: 'habits',
@@ -39,9 +64,19 @@ const habitSlice = createSlice({
     extraReducers: (builder) => {
         builder.addCase(fetchHabitsThunk.fulfilled, (state, action) => {
             state.habits = action.payload;
+        }).addCase(markAsDoneThunk.fulfilled, (state, action) => {
+            state.status[action.meta.arg] = 'success';
+            state.error[action.meta.arg] = null;
+        }).addCase(markAsDoneThunk.rejected, (state, action) => {
+            state.status[action.meta.arg] = 'failed';
+            state.error[action.meta.arg] = action.payload as string;
         })
     }
 });
 
 export const { getHabits, addHabit, removeHabit } = habitSlice.actions;
 export default habitSlice.reducer;
+
+function habitId(habitId: any) {
+    throw new Error('Function not implemented.');
+}
